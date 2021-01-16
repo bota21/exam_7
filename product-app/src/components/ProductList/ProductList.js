13ff19fdf338c99ab653ba913f2422789784f715
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-01-16 17:27:10 +0600

    #3 Добавлена сортировка

--- product-app/src/components/ProductList/ProductList.js
+++ product-app/src/components/ProductList/ProductList.js
@@ -1,25 +1,31 @@
 import { React } from 'react';
 import s from './ProductList.module.css';
 
-const ProductList = () => {
+const ProductList = props => {
     return (
         <div className={s.product_list}>
-            <div className={s.title_wrapper}>
-                <div className={s.title}>Title
-                <span className={s.triangle_up}></span>
-                </div>
-                <div className={s.price}>Price $
-                <div className={s.triangle_wrapper}>
-                <span className={s.triangle_up}></span>
-                <span className={s.triangle_down}></span>
-                </div>                
-                </div>
-                <div className={s.stock}>Stock
-                <span className={s.triangle_down}></span>
-                </div>
-            </div>           
+        <div className={s.title_wrapper}>
+        <div className={s.title}>Title
+        <div className={s.triangle_wrapper}>
+        <span className={s.triangle_up} onClick={props.sortNameUp}></span>
+        <span className={s.triangle_down} onClick={props.sortNameDown}></span>
+        </div>   
         </div>
-    )
-};
-
-export default ProductList;
+        <div className={s.price}>Price $
+        <div className={s.triangle_wrapper}>
+        <span className={s.triangle_up} onClick={props.sortPriceUp}></span>
+        <span className={s.triangle_down} onClick={props.sortPriceDown}></span>
+        </div>                
+        </div>
+        <div className={s.stock}>Stock
+        <div className={s.triangle_wrapper}>
+        <span className={s.triangle_up}  onClick={props.sortStockUp}></span>
+        <span className={s.triangle_down} onClick={props.sortStockDown}></span>
+        </div>    
+        </div>
+        </div>           
+        </div>
+        )
+    };
+    
+    export default ProductList;
